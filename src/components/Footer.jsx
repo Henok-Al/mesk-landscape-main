33ba86668f23288f0caa8c06afc05c6fcf41bd6b
Henok-Al <henok.gebresenbet@gmail.com>
2024-05-26 08:10:25 +0300

    some change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,20 +14,18 @@ const Footer = () => {
             <p className="mb-2 flex items-center">
               <FaPhone className="mr-2" /> (317) 748-9745
             </p>
-            
           </div>
           <div className="mb-8 md:mb-0 ">
-            <h3 className="text-2xl font-bold mb-4">
-              About Green Thumb Gardens
-            </h3>
+            <h3 className="text-2xl font-bold mb-4">About Mesk Landscaping</h3>
             <p className="leading-6 max-w-4xl">
               At Mesk Landscaping, we are passionate about creating beautiful
               and sustainable landscapes that enhance the natural beauty of your
-              property. <br />Our mission is to bring your outdoor vision to life with
-              creativity, expertise, and attention to detail.<br /> Whether you need a
-              complete landscape design, garden maintenance, or hardscaping
-              services, we have the skills and experience to exceed your
-              expectations.
+              property. <br />
+              Our mission is to bring your outdoor vision to life with
+              creativity, expertise, and attention to detail.
+              <br /> Whether you need a complete landscape design, garden
+              maintenance, or hardscaping services, we have the skills and
+              experience to exceed your expectations.
             </p>
           </div>
         </div>
