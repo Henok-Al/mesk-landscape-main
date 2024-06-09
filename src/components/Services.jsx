bb1ff73692df825370ea25a29802b7e30357f4fd
Henok-Al <henok.gebresenbet@gmail.com>
2024-06-09 12:38:21 +0300

    service img added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,25 +4,25 @@ const Services = () => {
   // Array of service objects
   const services = [
     {
-      image: "./images/pic1.jpg",
+      image: "./images/a.jpg",
       text: "Landscape Design",
       description:
-        "We specialize in creating stunning landscape designs tailored to your preferences and needs.",
+        " Our expert designers will work with you to create a unique and beautiful landscape that reflects your personal style and enhances the natural beauty of your property",
     },
     {
-      image: "./images/pic2.jpg",
+      image: "./images/d.jpg",
       text: "Garden Maintenance",
       description:
-        "We provide regular and seasonal property maintenance for commercial and residential gardens.",
-    },
-    {
-      image: "./images/pic3.jpg",
-      text: "Tree Trimming",
-      description:
-        "Our expert team offers professional tree trimming services to keep your trees healthy and beautiful.",
+        " Keep your garden looking its best with our comprehensive maintenance services",
     },
     {
       image: "./images/pic1.jpg",
+      text: "Hardscaping",
+      description:
+        "From patios and walkways to retaining walls and outdoor kitchens, we offer a wide range of hardscaping services to complement your landscape.",
+    },
+    {
+      image: "./images/c.jpg",
       text: "Irrigation Installation",
       description:
         "We install efficient irrigation systems to ensure your plants and lawn receive the right amount of water.",
@@ -40,7 +40,7 @@ const Services = () => {
         "We provide regular and seasonal property maintenance for commercial and residential gardens.",
     },
     {
-      image: "./images/pic1.jpg",
+      image: "./images/b.jpg",
       text: "Mulch Installation",
       description:
         "Professionally installed mulch for healthier, more beautiful landscapes. Enhances soil, retains moisture, suppresses weeds, and promotes plant growth.",
@@ -70,7 +70,7 @@ const Services = () => {
             key={index}
             className="max-w-xs rounded overflow-hidden shadow-xg relative mx-auto"
           >
-            <img className="w-full" src={service.image} alt={service.text} />
+            <img className=" h-[200px] w-[250px]" src={service.image} alt={service.text} />
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100">
               <p className="text-white text-center text-lg font-semibold transform transition-transform duration-300 hover:scale-110">
                 {service.description}
